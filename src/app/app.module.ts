62956b38310943ad982a19c809da737fdfe4e220
lionel jospin <yamkamlionel@yahoo.fr>
2022-04-18 03:05:52 +0200

    creeation des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JeuxComponent } from './jeux/jeux.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JeuxComponent
   ],
   imports: [
     BrowserModule,
